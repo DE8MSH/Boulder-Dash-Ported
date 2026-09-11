5b28010c141b12d74e558f9735a755ca0d61ef4f
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-11 20:39:02 +0200

    Add Cave 4 timing and exit progression

--- src/common/game_progress.s
+++ src/common/game_progress.s
@@ -19,13 +19,16 @@
 .export game_cave_complete
 .export game_cave_time
 
-CAVE_TIME_SECONDS = 150
+CAVE123_TIME_SECONDS = 150
+CAVE4_TIME_SECONDS   = 120
 CAVE1_EXIT_X      = $26
 CAVE1_EXIT_Y      = $12
 CAVE2_EXIT_X      = $12
 CAVE2_EXIT_Y      = $16
 CAVE3_EXIT_X      = $27
 CAVE3_EXIT_Y      = $14
+CAVE4_EXIT_X      = $26
+CAVE4_EXIT_Y      = $16
 FRAMES_PER_SECOND = 60
 DEATH_WAIT_FRAMES = 60
 
@@ -41,7 +44,14 @@ game_cave_time:           .res 1
     stz game_progress_frame
     stz game_progress_death_wait
     stz game_cave_complete
-    lda #CAVE_TIME_SECONDS
+    lda game_current_cave
+    cmp #4
+    bne :+
+    lda #CAVE4_TIME_SECONDS
+    bra @store_time
+:
+    lda #CAVE123_TIME_SECONDS
+@store_time:
     sta game_cave_time
     rts
 .endproc
@@ -99,6 +109,8 @@ game_cave_time:           .res 1
     beq @check_cave2_exit
     cmp #3
     beq @check_cave3_exit
+    cmp #4
+    beq @check_cave4_exit
 
     lda game_player_x
     cmp #CAVE1_EXIT_X
@@ -136,6 +148,19 @@ game_cave_time:           .res 1
     beq :+
     jmp @count_time
 :
+    jmp @cave_finished
+
+@check_cave4_exit:
+    lda game_player_x
+    cmp #CAVE4_EXIT_X
+    beq :+
+    jmp @count_time
+:
+    lda game_player_y
+    cmp #CAVE4_EXIT_Y
+    beq :+
+    jmp @count_time
+:
 
 @cave_finished:
     lda game_cave_time
@@ -143,7 +168,7 @@ game_cave_time:           .res 1
     stz game_cave_time
 
     lda game_current_cave
-    cmp #3
+    cmp #4
     bcs @final_hold
 
     jsr game_flow_next_cave
