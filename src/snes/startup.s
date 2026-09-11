485b19a5bbaddfa9311b366cd17feda379011825
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-11 09:16:43 +0200

    Add SNES reset startup and vectors

---
+++ src/snes/startup.s
@@ -0,0 +1,62 @@
+.setcpu "65816"
+
+.import game_init
+.import game_tick
+
+.segment "STARTUP"
+
+.proc reset
+    sei
+    clc
+    xce
+
+    rep #$30
+    lda #$0000
+    tcd
+    ldx #$1FFF
+    txs
+    sep #$30
+
+    jsr game_init
+
+@main:
+    jsr game_tick
+    bra @main
+.endproc
+
+.proc default_irq
+    rti
+.endproc
+
+.segment "HEADER"
+    .byte "BOULDER DASH PORT    " ; 21 bytes
+    .byte $20                   ; LoROM, slow ROM
+    .byte $00                   ; ROM only
+    .byte $05                   ; 32 KiB ROM
+    .byte $00                   ; no SRAM
+    .byte $02                   ; Europe/PAL region code
+    .byte $00                   ; licensee
+    .byte $00                   ; version
+    .word $FFFF                 ; checksum complement (placeholder)
+    .word $0000                 ; checksum (placeholder)
+
+.segment "VECTORS"
+    ; Native mode vectors ($FFE0-$FFEF)
+    .word $0000
+    .word $0000
+    .word default_irq           ; COP
+    .word default_irq           ; BRK
+    .word default_irq           ; ABORT
+    .word default_irq           ; NMI
+    .word $0000                 ; reserved
+    .word default_irq           ; IRQ
+
+    ; Emulation mode vectors ($FFF0-$FFFF)
+    .word $0000
+    .word $0000
+    .word default_irq           ; COP
+    .word $0000                 ; reserved
+    .word default_irq           ; ABORT
+    .word default_irq           ; NMI
+    .word reset                 ; RESET
+    .word default_irq           ; IRQ/BRK
